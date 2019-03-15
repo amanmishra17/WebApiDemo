7a76d3e425a3a872f971de4b600f29f0af5b4377
amanmishra17 <42470428+amanmishra17@users.noreply.github.com>
2019-03-16 02:02:18 +0530

    WebApi project created and performed basic opertaions

---
+++ webapi/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace webapi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<tblEmp>Get()
+        {
+            using (dbEmployeeEntities entities = new dbEmployeeEntities())
+            {
+                return entities.tblEmps.ToList();
+            }
+        }
+        public tblEmp Get(int id)
+        {
+            using (dbEmployeeEntities entities = new dbEmployeeEntities())
+            {
+                return entities.tblEmps.FirstOrDefault(e=>e.EmpId==id);
+            }
+        }
+    }
+}
