f1d18d2e3e789528967178e287ddad1f66c910cd
amanmishra17 <42470428+amanmishra17@users.noreply.github.com>
2019-03-17 01:57:17 +0530

    get and post opertions performed with status code

--- webapi/Controllers/EmployeeController.cs
+++ webapi/Controllers/EmployeeController.cs
@@ -10,18 +10,45 @@ namespace webapi.Controllers
 {
     public class EmployeeController : ApiController
     {
-        public IEnumerable<tblEmp>Get()
+        public IEnumerable<tblEmp> Get()
         {
             using (dbEmployeeEntities entities = new dbEmployeeEntities())
             {
                 return entities.tblEmps.ToList();
             }
         }
-        public tblEmp Get(int id)
+        public HttpResponseMessage Get(int id)
         {
             using (dbEmployeeEntities entities = new dbEmployeeEntities())
             {
-                return entities.tblEmps.FirstOrDefault(e=>e.EmpId==id);
+                var entity =  entities.tblEmps.FirstOrDefault(e => e.EmpId == id);
+                if(entity!=null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "employee with id = "+id.ToString() + "not found");
+                }
+            }
+
+        }
+        public HttpResponseMessage Post([FromBody] tblEmp employee)
+        {   try
+            {
+                using (dbEmployeeEntities entities = new dbEmployeeEntities())
+                {
+                    entities.tblEmps.Add(employee);
+                    entities.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.EmpId.ToString());
+                    return message;
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
     }
